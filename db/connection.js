753d1a7d3ff3359a30a4c848a874fb44c971248d
ByeolNabi <narwhal_blast@naver.com>
2025-04-19 21:30:43 +0900

    서버 배포를 위한 docker, 및 origin값 수정

--- db/connection.js
+++ db/connection.js
@@ -2,13 +2,21 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: 'localhost',
+  host: 'db',
   user: 'root', // 데이터베이스 사용자명
   password: 'root', // 데이터베이스 비밀번호
   database: 'staff_tracker', // 데이터베이스 이름
   waitForConnections: true,
+  port : 3306,
   connectionLimit: 100,
   queueLimit: 0
 });
 
 module.exports = { pool };
+
+// docker run --name mysql-container -e MYSQL_ROOT_PASSWORD=root -d -p 3307:3306 mysql:latest
+// mysql -u root -p
+// CREATE DATABASE staff_tracker;
+
+// docker build -t node-api:latest .
+// docker run --name node-container -d -p 3306:3000 node-api:latest
